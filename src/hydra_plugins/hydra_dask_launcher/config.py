f6f067b73c73e07f050265ef2ee3a4e0c90342a2
Quentin Febvre <quentin.febvre@imt-atlantique.fr>
2025-07-15 13:30:50 +0000

    add default cluster

--- src/hydra_plugins/hydra_dask_launcher/config.py
+++ src/hydra_plugins/hydra_dask_launcher/config.py
@@ -4,6 +4,17 @@ from dask.distributed import Client
 
 from hydra.core.config_store import ConfigStore
 
+parallel_config = {
+    "_target_": "joblib.parallel_config",
+    "backend": "dask",
+    "verbose": 0,
+}
+cluster = {
+    "_target_": "dask.distributed.LocalCluster",
+}
+client = {
+    "_target_": "dask.distributed.Client",
+}
 
 @dataclass
 class DaskLauncherConf:
@@ -11,28 +22,17 @@ class DaskLauncherConf:
 
     _target_: str = "hydra_plugins.hydra_dask_launcher.launcher.DaskLauncher"
     verbose: int = 0
-    cluster: Optional[Any] = None
+    cluster: Optional[Any] = field(default_factory=lambda: cluster)
     scale_kwargs: Optional[Dict[str, Any]] = None
-    client: Optional[Dict[str, Any]] = None
+    client: Optional[Dict[str, Any]] = field(default_factory=lambda: client)
     adapt_kwargs: Optional[Dict[str, Any]] = None
-    parallel_config: Optional[Dict[str, Any]] = None
+    parallel_config: Optional[Dict[str, Any]] = field(default_factory=lambda: parallel_config)
     _recursive_: bool = False
 
 
 def setup_config() -> None:
     """Set up the configuration store for the Joblib launcher."""
     cs = ConfigStore.instance()
-    parallel_config = {
-        "_target_": "joblib.parallel_config",
-        "backend": "dask",
-        "verbose": 0,
-    }
-    cluster = {
-        "_target_": "dask.distributed.LocalCluster",
-    }
-    client = {
-        "_target_": "dask.distributed.Client",
-    }
     cs.store(
         group="hydra/launcher",
         package="hydra.launcher",
